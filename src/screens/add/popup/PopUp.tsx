f4da3693a9e5fcdef507c2183e88b467c6d91beb
Pmmvito <pmmvito@hotmail.com>
2024-04-06 16:20:49 -0300

    PopUp e Modal De opções do ADD

--- src/screens/add/popup/PopUp.tsx
+++ src/screens/add/popup/PopUp.tsx
@@ -1,26 +1,46 @@
-import { StyleSheet, Text, View, TextInput} from 'react-native';
-export default function TelaAdd(){
-return(
- <View style={styles.conteiner}>
-   <Text style= {styles.popUp}>
-   </Text>
-    <TextInput style={styles.input}></TextInput>
- </View>
-);
-}
+import React, { useState } from 'react';
+import { View, Text, Modal, TouchableOpacity, StyleSheet, Button } from 'react-native';
+
+const MeuModal = ({ visible, onClose }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+    >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.conteiner}>
+          <TouchableOpacity onPress={() => onClose('Opção 1')}>
+          <Button title="Fechar" onPress={() => (onClose)} />
+          </TouchableOpacity>
+          <Text>Escolha uma opção:</Text>
+          <TouchableOpacity onPress={() => onClose('Opção 1')}>
+            <Text>Opção 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onClose('Opção 2')}>
+            <Text>Opção 2</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default MeuModal;
 const styles = StyleSheet.create({
   
     conteiner: {
         flex: .2,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: 'white', 
+        padding: 20, 
+        borderRadius: 10 
       },
       popUp:{
         fontSize:24,
         fontWeight: 'bold',
       },
-      input:{
-        backgroundColor: 'white',
-        width: '90%',
-      }
+    
 })
