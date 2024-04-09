3ebdccf5b614e7d8cfa7ee232d7eb0b105037b6c
Pmmvito <pmmvito@hotmail.com>
2024-04-09 18:35:12 -0300

    Nome e Email agora É puxado

--- src/screens/login/Dashboard.tsx
+++ src/screens/login/Dashboard.tsx
@@ -12,13 +12,23 @@ import React, { useState, useEffect } from "react";
 import Colors from "../../../constants/Colors";
 import { Feather } from "@expo/vector-icons";
 import { auth, db } from "../../../firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, where } from "firebase/firestore";
+import { getAuth, signOut, } from "firebase/auth";
+import { isEqualsGreaterThanToken } from "typescript";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Dashboard({ navigation }: { navigation: any }) {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
   const [userInfo, setUserInfo] = useState<any | undefined>(null);
-
   const handleSignout = async () => {
-    await auth.signOut();
+
+
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
   };
   const Modal = () => {
     Alert.alert("Auth App", "Do you really want to logout", [
@@ -31,24 +41,42 @@ export default function Dashboard({ navigation }: { navigation: any }) {
   };
 
   const getData = async () => {
-    const docRef = doc(db, "users", "info");
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      setUserInfo(docSnap.data());
-    } else {
-      console.log("No such document!");
-    }
-  };
+    const auth = getAuth();
+    const user = auth.currentUser;
+if (user) {
+ // Recupere o documento do usuário no Firestore
+ const userDocRef = doc(db, 'users', user.uid); // Substitua 'Users' pelo nome da sua coleção
+ const userDocSnap = await getDoc(userDocRef);
+
+ if (userDocSnap.exists()) {
+   const userData = userDocSnap.data();
+   setUserName(userData?.Name); // Ajuste o nome do campo conforme sua estrutura de dados
+   console.log('Nome do usuário:', userName);
+   // Agora você pode usar o nome do usuário onde precisar
+ } else {
+   console.log('Documento do usuário não encontrado.');
+ }
+} else {
+ console.log('Usuário não autenticado.');
+}
+ // Get the current user
+    if (user) {
+      // Fetch the user's email
+      setEmail(user.email);
+      console.log(email)
+}}
 
   useEffect(() => {
     getData();
   }, []);
   return (
+    
     <View style={styles.container}>
+      <Text style={{ fontSize: 25 }}>{userName}</Text>
+      <Text style={{ fontSize: 25 }}>{email}</Text>
       <Text style={{ fontSize: 25 }}>Welcome Fam!</Text>
       <View>
-        <TouchableOpacity style={styles.button} onPress={Modal}>
+        <TouchableOpacity style={styles.button} onPress={handleSignout}>
           <Text style={{ color: Colors.white, fontSize: 20 }}>Sign out</Text>
         </TouchableOpacity>
       </View>
