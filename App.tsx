cb65a35e361945d2546b1f0cf6a7a9035fb685a7
Pablo Garcia <pablo.pmza@gmail.com>
2024-04-10 05:53:25 -0300

    Correção package

--- App.tsx
+++ App.tsx
@@ -3,12 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation/Navigation';
 
 export default function App() {
-  return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!!!!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Navigation/>
+  return ( 
+   <Navigation/>
   );
 }
 
